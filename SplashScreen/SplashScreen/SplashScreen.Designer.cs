e93f45ba69b5ca497b7947ead42e6f37af483499
Georgios Psarakis <g.psarakis3800@gmail.com>
2016-09-22 20:52:03 -0400

    RADical lab push

---
+++ SplashScreen/SplashScreen/SplashScreen.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SplashScreen
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.splashLabel = new System.Windows.Forms.Label();
+            this.SplashTimer = new System.Windows.Forms.Timer(this.components);
+            this.SplashBackGround = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SplashBackGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splashLabel
+            // 
+            this.splashLabel.AutoSize = true;
+            this.splashLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splashLabel.Location = new System.Drawing.Point(29, 149);
+            this.splashLabel.Name = "splashLabel";
+            this.splashLabel.Size = new System.Drawing.Size(268, 55);
+            this.splashLabel.TabIndex = 0;
+            this.splashLabel.Text = "Cannonball";
+            this.splashLabel.Click += new System.EventHandler(this.splashLabel_Click);
+            // 
+            // SplashTimer
+            // 
+            this.SplashTimer.Enabled = true;
+            this.SplashTimer.Interval = 3000;
+            this.SplashTimer.Tick += new System.EventHandler(this.SplashTimer_Tick);
+            // 
+            // SplashBackGround
+            // 
+            this.SplashBackGround.Image = ((System.Drawing.Image)(resources.GetObject("SplashBackGround.Image")));
+            this.SplashBackGround.Location = new System.Drawing.Point(0, 12);
+            this.SplashBackGround.Name = "SplashBackGround";
+            this.SplashBackGround.Size = new System.Drawing.Size(338, 199);
+            this.SplashBackGround.TabIndex = 1;
+            this.SplashBackGround.TabStop = false;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 213);
+            this.Controls.Add(this.splashLabel);
+            this.Controls.Add(this.SplashBackGround);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.Text = "Splash Screen";
+            ((System.ComponentModel.ISupportInitialize)(this.SplashBackGround)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label splashLabel;
+        private System.Windows.Forms.Timer SplashTimer;
+        private System.Windows.Forms.PictureBox SplashBackGround;
+    }
+}
+
---
+++ SplashScreen/SplashScreen/SplashScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreen
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashTimer_Tick(object sender, EventArgs e)
+        {
+            SplashTimer.Enabled = false;
+            this.Close();
+        }
+
+        private void splashLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
